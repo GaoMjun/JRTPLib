0865443e70b435b81865eb7bd854d2e3201806f0
GaoMjun <jun2675731483@gmail.com>
2017-04-17 23:12:05 +0800

    JRTPLib-jni

---
+++ app/src/main/cpp/jrtplib-jni.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Mannix on 4/17/17.
+//
+
+#include <jni.h>
+
+static const char *TAG = "JRTPLib-jni";
+#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
